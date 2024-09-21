03a9883362e18143f1227d984d225a0363240e0c
yrranjay <yrranjayg@gmail.com>
2024-09-21 16:40:45 +0800

    add models

---
+++ Data/MovieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieCinema.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy = 2,
+        Drama = 3,
+        Documentary = 4,
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using MovieCinema.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieCinema.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Logo { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory Category { get; set; }
+    }
+}
